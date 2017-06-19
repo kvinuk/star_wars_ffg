5e2dbed010d6ad303c0c9afa894473a28168786e
kvinuk <kevinomuro@gmail.com>
2017-06-19 15:16:14 -0500

    Improved user and user_spec to make no offenses

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    let(:user) { FactoryGirl.build(:user) }
-    subject { user }
+  let(:user) { FactoryGirl.build(:user) }
+  subject { user }
 
-    it { should respond_to(:name) }
+  it { should respond_to(:name) }
 end
